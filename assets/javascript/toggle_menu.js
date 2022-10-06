fb639e4ebbffd0772236e6993b624eae21decb2a
André Reis <andresreis.dev@gmail.com>
2022-10-06 19:54:49 -0300

    update 06/10/2022 19:53
    * Problema de responsividade do menu resolvido;
    * Mudanças

--- assets/javascript/toggle_menu.js
+++ assets/javascript/toggle_menu.js
@@ -3,10 +3,12 @@ let menuStatus = false; //define o status do menu para possibilitar a manipulaç
 
 function toggleMenu () {
     if (menuStatus == false) {
-        menu.style.display = 'block' //define o display do menu para block
+        menu.style.height = '95px' //define o display do menu para block
+        menu.style.animationName = 'menuDown' //Ativa a animação que faz com que o menu desça devagar
         menuStatus = true; //converte a variável de status para "true" 
     } else {
-        menu.style.display = 'none' //define o display do menu para "none"
+        menu.style.height = '0px' //define o display do menu para "none"
+        menu.style.animationName = 'menuUp' //Ativa a animação que faz com que o menu suba devagar
         menuStatus = false; //converte a variável de status para "false" 
     }
 }
