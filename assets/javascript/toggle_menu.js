115a3b27c953e8b29315ef73a6357a43ccec3ff1
André Reis <andresreis.dev@gmail.com>
2022-10-05 22:51:09 -0300

    update 02/10/2022

---
+++ assets/javascript/toggle_menu.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById('menu') //variável referente ao menu, o elemento <ul> do HTML
+let menuStatus = false; //define o status do menu para possibilitar a manipulação do menu na função
+
+function toggleMenu () {
+    if (menuStatus == false) {
+        menu.style.display = 'block' //define o display do menu para block
+        menuStatus = true; //converte a variável de status para "true" 
+    } else {
+        menu.style.display = 'none' //define o display do menu para "none"
+        menuStatus = false; //converte a variável de status para "false" 
+    }
+}
