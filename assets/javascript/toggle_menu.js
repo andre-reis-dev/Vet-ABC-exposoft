d4cba36589897e859f006e14114677cfa6570051
André Reis <andresreis.dev@gmail.com>
2022-10-09 20:39:40 -0300

    update 09/10/2022 20:40
    * Correção do problema do Menu
    * Atualização do menu para as outras páginas

--- assets/javascript/toggle_menu.js
+++ assets/javascript/toggle_menu.js
@@ -1,14 +1,12 @@
-let menu = document.getElementById('menu') //variável referente ao menu, o elemento <ul> do HTML
+let menu = document.getElementById('menu-mobile') //variável referente ao menu, o elemento <ul> do HTML
 let menuStatus = false; //define o status do menu para possibilitar a manipulação do menu na função
 
 function toggleMenu () {
     if (menuStatus == false) {
-        menu.style.height = '95px' //define o display do menu para block
-        menu.style.animationName = 'menuDown' //Ativa a animação que faz com que o menu desça devagar
+        menu.style.display = 'block' //define o display do menu para block
         menuStatus = true; //converte a variável de status para "true" 
     } else {
-        menu.style.height = '0px' //define o display do menu para "none"
-        menu.style.animationName = 'menuUp' //Ativa a animação que faz com que o menu suba devagar
+        menu.style.display = 'none' //define o display do menu para "none"
         menuStatus = false; //converte a variável de status para "false" 
     }
 }
