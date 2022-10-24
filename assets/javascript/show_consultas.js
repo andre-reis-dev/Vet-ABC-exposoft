9412e93adfa20b553b85ce195b18725a483d581a
André Reis <112898914+andre-reis-dev@users.noreply.github.com>
2022-10-24 12:16:30 -0300

    update 24/10/2022
    * Finalização do layout da agenda dos funcionários;
    * Início do script da página;

---
+++ assets/javascript/show_consultas.js
@@ -0,0 +1,9 @@
+let eventContents = document.getElementsByClassName('event-content');
+let buttonShowEvent = document.getElementsByClassName('show-buttons');
+let eventVisibility = false;
+
+function showEvent (n) {
+    if (eventVisibility = false) {
+        eventContents[n].style.display = 'block';
+    }
+}
