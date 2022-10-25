f897d5599bebc8966ef4eb66908244b7943a81b8
camilafreo <camila.afanasiev@scseduca.com.br>
2022-10-25 19:14:57 -0300

    25-10-22
    PHP DO CADASTRO

--- assets/php/cadastro_cliente.php
+++ assets/php/cadastro_cliente.php
@@ -1,33 +1,32 @@
 
-
 <?php
 
 if (isset($_POST["cadastrar"]))
 {
     $conexao=mysqli_connect("localhost","root","","vet-abc");
-<<<<<<< Updated upstream
     $nome=$_POST['nome'];
     $cpf=$_POST['cpf'];
     $telefone=$_POST['telefone'];
     $email=$_POST['email'];
     $senha=$_POST['senha'];
-=======
-    $nome=$_POST["nome"];
-    $cpf=$_POST["cpf"];
-    $telefone=$_POST["telefone"];
-    $email=$_POST["email"];
-     $senha=$_POST["senha"];
-     
->>>>>>> Stashed changes
-    
-    $sql= "INSERT INTO cadastro (nome, cpf, telefone, email, senha) VALUES ('$nome','$cpf', '$telefone', '$email','$senha')";
-    
-   $result=mysqli_query($conexao,$sql);
+    $conf_senha = $_POST['conf_senha'];
 
-<<<<<<< Updated upstream
-   header("Location: ../html/login.php");
-=======
-   header ("location: ../html/loguin.php")
->>>>>>> Stashed changes
+    if ($conf_senha != $senha) {
+        echo "oi!";
+        #echo "<script>alert('srfref')</script>";
+        echo "<script>alert('Senhas não conferem, por favor, insira novamente!'); window.location='../html/cadastro.php'</script>";
+        #header ('location: ../html/cadastro.php');
+    } else {
+        $sql= "INSERT INTO cadastro (nome, cpf, telefone, email, senha) VALUES ('$nome','$cpf', '$telefone', '$email','$senha')";
+    
+        $result=mysqli_query($conexao,$sql);
+
+        header("Location: ../html/login.php");
+    }
+    
+    
+   
+
+   
 }
 ?>
