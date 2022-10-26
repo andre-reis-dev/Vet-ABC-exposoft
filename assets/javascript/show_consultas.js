b6dd5f6aea51dd34c689c4294de96235a871481c
André Reis <112898914+andre-reis-dev@users.noreply.github.com>
2022-10-25 21:56:37 -0300

    update 25/10/2022 21:56
    * Finalização do script da agenda de funcionários

--- assets/javascript/show_consultas.js
+++ assets/javascript/show_consultas.js
@@ -1,6 +1,9 @@
 let date = new Date(); // A variável "date" recebe o objeto date(), que possibilita a manipulação de datas e horários da máquina
+
 let monthNumber = date.getMonth(); // Recebe o atributo "getMonth()" do objeto "date()", ou seja, recebe o número do mês atual (vale lembrar que o primeiro mês começa no valor "0" e não "1")
+
 let monthName = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]; // Vetor que contém os nomes dos meses em ordem, também contendo o primeiro valor na posição 0 do vetor
+
 let monthBanner = document.getElementById('monthBanner'); 
 let monthIndex = monthNumber; // monthIndex recebe o número de monthNumber, ou seja, o mês decorrente
 
@@ -19,26 +22,29 @@ function toggleMonth (value) {
         }
     }
     monthBanner.innerHTML = `<strong>${monthName[monthIndex]}</strong>` ;
-    console.log(monthIndex);
+    console.log(monthIndex); //escreve no console do navegador o número do mês atual
 }
 
 
-let eventContents = document.getElementsByClassName('event-content');
-let buttonShowEvent = document.getElementsByClassName('show-buttons');
-let eventVisibility = false;
-eventVisibility.length = eventContents.length
+let eventContents = document.getElementsByClassName('event-content'); //recebe todos os elementos com classe 'event-content'
+let buttonShowEvent = document.getElementsByClassName('show-buttons'); // recebe todos os elementos com classe 'show-buttons'
+let eventVisibility = false; //variável de controle de visibilidade das consultas
 let angle = 0;
 
 function showEvent (n) {
-    if (eventVisibility == false) {
-        eventContents[n].style.display = 'block'
+    if (eventVisibility == false) { // verificação: se a variável "event-visibility" for igual "false"
+        eventContents[n].style.display = 'block' 
         eventVisibility = true;
-        buttonShowEvent[n].style.transform = `rotate(180deg)`;
-        buttonShowEvent[n].style.transition = '.5s';
+        buttonShowEvent[n].style.transform = `rotate(${angle += 180}deg)`; //define  o atributo "transform" do elemento para rotate, recebendo o valor da variável angle + 180 graus, para que a rotação do elemento não volte, mas sempre prossiga.
+        buttonShowEvent[n].style.transition = '.5s'; //define o atributo transition do elemento para 0.5 segundos.
     } else {
         eventContents[n].style.display = 'none'
         eventVisibility = false;
-        buttonShowEvent[n].style.transform = `rotate(360deg)`;
+        buttonShowEvent[n].style.transform = `rotate(${angle += 180}deg)`;
         buttonShowEvent[n].style.transition = '.5s';
     }
 }
+/*
+    A função acima (showEvent) é responsável por alternar a visibilidade das consultas, através do click do top do elemento.
+    Ele também faz com que a setinha do botão mude de posição, para indicar se deve subir ou descer.
+*/
