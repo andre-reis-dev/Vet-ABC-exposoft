818ca91b7e3448e6671cf0df2cc273f9e7a8fad1
camilafreo <camila.afanasiev@scseduca.com.br>
2022-10-26 22:18:12 -0300

    26-10-22
    ATUALIAÇÃO DAS PÁGINAS

--- assets/html/cadastro.php
+++ assets/html/cadastro.php
@@ -4,56 +4,58 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
         <meta name="viewport" charset="utf-8"> <!-- IMG DO TRIBAR -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="style-cadastro.css">
-        <link rel="stylesheet" href="style-global.css">
-        <link rel="stylesheet" href="rodape.css">
+        <link rel="stylesheet" href="../css/style-cadastro.css">
+        <link rel="stylesheet" href="../css/style-global.css">
+        <link rel="stylesheet" href="../css/rodape.css">
     </head>
     <body>       
         <header class="area-logo"><!-- Aqui fica a logo da página -->
             <img class="col-sm-5 col-md-3 col-lg-2" src="../img/logo.png" alt="logo da empresa vet abc">
         </header>
         
-        <nav class="main-nav-bar">
+        <nav class="main-nav-bar"><!-- ESSE MENU É PARA PC E TABLET -->
+            <span class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</span>
             <ul id="menu">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="agenda.php">Consulta</a></li>
-                <li><a href="quemsomos.php">Quem Somos</a></li>
-                <li><a class="login-cadastro logout" href="login.php">Login</a></li>
-                <li><a class="login-cadastro logout" href="cadastro.php">Cadastro</a></li>
+                <li><a href="../../index.html">Home</a></li>
+                <li><a href="quemsomos.html">Quem Somos</a></li>
+                <li><a href="agenda.php">Agenda</a></li>
+                <li><a class="login-cadastro" href="login.php">Login</a></li>
+                <li><a class="login-cadastro" href="cadastro.php">Cadastro</a></li>
             </ul>
-        
         </nav>
-        <nav class="nav-menu-mobile">
+
+        <nav class="nav-menu-mobile"> <!-- ESSE MENU É PARA CELULAR -->
             <label for="three-bars" id="three-bars" class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</label>
             <ul id="menu-mobile">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="agenda.php">Consulta</a></li>
-                <li><a href="quemsomos.php">Quem Somos</a></li>
-                <li><a class="login-cadastro logout" href="login.php">Login</a></li>
-                <li><a class="login-cadastro logout" href="cadastro.php">Cadastro</a></li>
-                <li><a class="login-cadastro sair" href="../php/logout.php">Sair</a></li>
+                <li><a href="../../index.html">Home</a></li>
+                <li><a href="quemsomos.html">Quem Somos</a></li>
+                <li><a href="agenda.php">Agenda</a></li>
+                <li><a class="login-cadastro" href="login.php">Login</a></li>
+                <li><a class="login-cadastro" href="cadastro.php">Cadastro</a></li>
             </ul>
         </nav>
         
         <div class="cadastro col-sm-8 col-md-6 col-lg-4"><!-- Essa div vai conter os cadastros -->
-            <form class="formulario" action="cadastro_cliente.php" method="post" name="formulario"> <!-- Cria um formulário -->
+            <form class="formulario" action="../php/cadastro_cliente.php" method="post" name="formulario"> <!-- Cria um formulário -->
                 <label>Nome Completo:</label><br>
-                <input class="resposta" type="text" name="nome" placeholder="Digite o nome"><br><br>
+                <input required class="resposta" type="text" name="nome" placeholder="Digite o nome" id="nome" ><br><br>
 
-                <label> CPF:</label><br>
-                <input class="resposta" type="text" name="cpf" placeholder="Digite o CPF"><br><br>
-
+                <label> CPF :</label><br>
+                <input required class="resposta" type="text" name="cpf" placeholder="Digite o CPF"   id="cpf" ><br><br>
+                <!-- required, mostra uma mensagem caso o campo não seja preenchido
+                    placeholder, uma mensagem que aparecera dentro da caixinha do usuário--> 
                 <label> Telefone:</label><br>
-                <input class="resposta" type="text" name="telefone" placeholder="Digite o telefone"><br><br>
+                <input required class="resposta" type="text"  name="telefone" id="telefone" placeholder="Digite o telefone">
 
+                <br><br>
                 <label>Email:</label><br>
-                <input class="resposta" type="text" name="email" placeholder="Digite o email"><br><br>
+                <input required class="resposta" type="text" name="email" placeholder="Digite o email" id="email"><br><br>
 
                 <label>Senha:</label><br>
-                <input class="resposta" type="password" name="senha" placeholder="Digite a senha"><br><br>
+                <input required class="resposta" type="password" name="senha" placeholder="Digite a senha" id="senha" ><br><br>
 
                 <label>Confirmação da Senha:</label><br>
-                <input class="resposta" type="password" name="conf_senha" placeholder="Confirme a senha"><br>
+                <input required class="resposta" type="password" name="conf_senha" placeholder="Confirme a senha" id="configsenha" ><br>
                 <!-- placeholder é o que aparece dentro da caixinha -->
 
                 <br><input class="botao" type="submit" name="cadastrar" value="cadastrar"> <!-- aqui eu crio um botão -->
@@ -73,8 +75,15 @@
             </div>
         </footer>
         <script src="../javascript/toggle_menu.js"></script>
+        <script type="text/javascript" src="jquery.js "></script>
+        <script type="text/javascript" src="jquerycfp.js"></script> 
+        <script src="../javascript/verificacao_cadastro.js"></script>
+       
+        
+  
     </body>
 </html>
 
 
+   
     
--- assets/php/cadastro_cliente.php
+++ assets/php/cadastro_cliente.php
@@ -11,22 +11,28 @@ if (isset($_POST["cadastrar"]))
     $senha=$_POST['senha'];
     $conf_senha = $_POST['conf_senha'];
 
-    if ($conf_senha != $senha) {
+    if ($conf_senha != $senha) {  #ver se as senhas conferem 
         echo "oi!";
-        #echo "<script>alert('srfref')</script>";
+
         echo "<script>alert('Senhas não conferem, por favor, insira novamente!'); window.location='../html/cadastro.php'</script>";
-        #header ('location: ../html/cadastro.php');
-    } else {
+        
+    } else {  
+            $sql_email = "SELECT * FROM cadastro WHERE email = '$email' ";
+            $result = mysqli_query($conexao,$sql_email);
+        
+            If (mysqli_num_rows($result) != 0){ 
+            
+              echo "<script> alert ('email existente')</script>";
+        } else {
+
         $sql= "INSERT INTO cadastro (nome, cpf, telefone, email, senha) VALUES ('$nome','$cpf', '$telefone', '$email','$senha')";
     
         $result=mysqli_query($conexao,$sql);
 
         header("Location: ../html/login.php");
+        }
     }
     
-    
    
-
-   
-}
+}   
 ?>
