3b3ad532a2165058002bf37288991ef29b66b1f1
camilafreo <camila.afanasiev@scseduca.com.br>
2022-11-11 19:07:58 -0300

    11-11-22
    atualização cadastro

--- assets/javascript/verificacao.js
+++ assets/javascript/verificacao.js
@@ -1,12 +1,24 @@
+
 $(document).ready(function(){
     $('#cpf').mask('999.999.999-99');
+    $('#telefone').mask('(99)99999-9999');
 });
+    
+ $(document).ready(function(){
+    $("#conf_senha").blur(function(e){ //aciona o evento blur ou anexa uma função para ser executada quando ocorre um evento blur.
+        //quando o usuário sair da caixinha da confirmação de senha, automaticamente será verificado se as senhas estão em conjunto
+        var senha = document.getElementById("senha").value;
+        var conf_senha = document.getElementById("conf_senha").value;
+        if (senha != conf_senha) { 
+            alert("senhas não compativeis");
+           return false;
+        } 
+    });
+ });
 
-$(document).ready(function(){
-    $('#telefone').mask('(99) 99999-9999');
-});
 
-const form = document.getElementById('form') // variável que contém todo o formulário
+ 
+    const form = document.getElementById('form') // variável que contém todo o formulário
 const cpf = document.getElementById('cpf')  // variável do CPF
 const telefone = document.getElementById('tel') // variável do telefone
 const nome = document.getElementById('nome')// variável do nome
@@ -24,9 +36,9 @@ form.addEventListener("submit", (e) => { // cria um evento chamado "submit", cuj
 function checkInputs() {
     const cpfValue = cpf.value; 
     const telefoneValue = telefone.value;
-    const nomeValue = nome.value;
+    const nomeValue = nome.value; 
     const emailValue = email.value;
-    const senhaValue = senha.value;
+    const senhaValue = senha.value; 
     const configValue = config.value;
     /*
         variáveis que recebem os valores digitados pelo usuário, uma para cada informação.
@@ -37,49 +49,53 @@ function checkInputs() {
     } else {
         if (cpfValue.length <= 10) {
             small[0].innerText = 'Por favor, digite um CPF válido' // se o campo de cpf receber um valor com menos de 11 caracteres, emite uma mensagem de erro, bem como feito acima.
+            
         } else {
             small[0].innerText = '' // senão, o small não aparece.
-        }
-    }
+                 }
+             }
 
     if (telefoneValue == "") {
         small[1].innerText = 'O campo Telefone é obrigatório'
+           
     } else {
         if (telefoneValue.length <= 10) {
             small[1].innerText = 'Por favor, digite um Telefone válido'
+            
         } else {
             small[1].innerText = ''
-        }
-    } 
+                }
+            } 
+
     if (nomeValue == ""){
         small[2].innerText = 'O campo do Nome é obrigatório'
+        
     }else{
-        if(nomeValue.length <= 0){
-            small[2].innerText = 'Por favor, digite seu Nome'
-        }else{
             small[2].innerText = ''
         }
-    }
+
     if (emailValue == ""){
         small[3].innerText = 'O campo do Email é obrigatório'
     }else{
-        if (emailValue.length <= 0){
-            small[3].innerText = 'Por favor, digite seu Email'
-        }else{
-            small[3].innerText = ''
+            smail[3].innerText = ''
         }
 
-    }
     if (senhaValue == ""){
     small[4].innerText = 'O campo da Sennha é obrgatório'
     }else{
-        if (senhaValue.length <= 0){
-            small[4].innerText= 'Pos favor, digite sua Senha'
-        }else{
             small[4].innerText = ''
         }
-    }
+    
     if (configValue == ""){
     small[5].innerText = 'O campo da Sennha é obrgatório'
-}
-}
+    }else{
+        small[5].innerText = ''
+        }
+};   
+
+
+
+
+
+
+
