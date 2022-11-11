ac8e671e57d74ac4382497aaff778c8db4188345
André Reis <112898914+andre-reis-dev@users.noreply.github.com>
2022-11-11 20:15:17 -0300

    layout update

--- assets/html/cadastro.php
+++ assets/html/cadastro.php
@@ -20,56 +20,65 @@
             <img class="col-sm-5 col-md-3 col-lg-2" src="../img/logo.png" alt="logo da empresa vet abc">
         </header>
         
-    <nav class="main-nav-bar"><!-- Essa div vai conter o MENU -->
-        <span class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</span>
-        <ul id="menu">
+        <nav class="main-nav-bar"><!-- Essa div vai conter o MENU -->
+            <span class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</span>
+            <ul id="menu">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="agenda.php">Consulta</a></li>
                 <li><a href="quemsomos.php">Quem Somos</a></li>
                 <li><a class="login-cadastro logout" href="login.php">Login</a></li>
                 <li><a class="login-cadastro logout" href="cadastro.php">Cadastro</a></li>
                 <li><a class="login-cadastro sair" href="../php/logout.php">Sair</a></li> 
-        </ul>
-    </nav>
-    <nav class="nav-menu-mobile">
-        <label for="three-bars" id="three-bars" class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</label>
-        <ul id="menu">
+            </ul>
+        </nav>
+        <nav class="nav-menu-mobile">
+            <label for="three-bars" id="three-bars" class="material-symbols-outlined three-bars col-sm-2" onclick="toggleMenu()">menu</label>
+            <ul id="menu">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="agenda.php">Consulta</a></li>
                 <li><a href="quemsomos.php">Quem Somos</a></li>
                 <li><a class="login-cadastro logout" href="login.php">Login</a></li>
                 <li><a class="login-cadastro logout" href="cadastro.php">Cadastro</a></li>
                 <li><a class="login-cadastro sair" href="../php/logout.php">Sair</a></li> 
-        </ul>
-    </nav>
-        
-        <div class="cadastro col-sm-8 col-md-6 col-lg-4"><!-- Essa div vai conter os cadastros -->
-            <form class="formulario" action="../php/cadastro_cliente.php" method="post" name="formulario"> <!-- Cria um formulário -->
-                <label>Nome Completo:</label><br>
-                <input required class="resposta" type="text" name="nome" placeholder="Digite o nome" id="nome" ><br><br>
-                    <!-- required: especifica que um campo de entrada deve ser preenchido antes de enviar o formulário. -->
-                <label> CPF :</label><br>
-                <input  required class="resposta" type="text" name="cpf" placeholder="Digite o CPF" id="cpf"><br><br>
+            </ul>
+        </nav>
 
-                <label> Telefone:</label><br>
+        <form class="formulario" action="../php/cadastro_cliente.php" method="post" name="formulario"> <!-- Cria um formulário -->
+            <h1>Cadastro</h1>
+
+            <div>
+                <label>Nome Completo:</label>
+                <input required class="resposta" type="text" name="nome" placeholder="Digite o nome" id="nome">
+            </div>
+                <!-- required: especifica que um campo de entrada deve ser preenchido antes de enviar o formulário. -->
+            <div>
+                <label> CPF :</label>
+                <input  required class="resposta" type="text" name="cpf" placeholder="Digite o CPF" id="cpf">
+            </div>
+
+            <div>
+                <label> Telefone:</label>
                 <input required class="resposta" type="text"  name="telefone" id="telefone" placeholder="Digite o telefone">
+            </div>
 
-                <br><br>
-                <label>Email:</label><br>
-                <input required class="resposta" type="text" name="email" placeholder="Digite o email" id="email"><br><br>
+            <div>
+                <label>Email:</label>
+                <input required class="resposta" type="text" name="email" placeholder="Digite o email" id="email">
+            </div>
 
-                <label>Senha:</label><br>
-                <input required class="resposta" type="password" name="senha" placeholder="Digite a senha" id="senha"><br><br>
+            <div>
+                <label>Senha:</label>
+                <input required class="resposta" type="password" name="senha" placeholder="Digite a senha" id="senha">
+            </div>
 
-                <label>Confirmação da Senha:</label><br>
-                <input required class="resposta" type="password" name="conf_senha" placeholder="Confirme a senha" id="conf_senha"><br>
-                <!-- placeholder é o que aparece dentro da caixinha -->
+            <div>
+                <label>Confirmação da Senha:</label>
+                <input required class="resposta" type="password" name="conf_senha" placeholder="Confirme a senha" id="conf_senha">
+            </div>
+            <!-- placeholder é o que aparece dentro da caixinha -->
 
-                <br><input class="botao" type="submit" name="cadastrar" value="cadastrar"> <!-- aqui eu crio um botão -->
-
-                
-            </form>
-        </div>
+            <input class="botao" type="submit" name="cadastrar" value="Cadastrar"> <!-- aqui eu crio um botão -->
+        </form>
 
         <footer class="rodape col-sm-12 col-md-12 col-lg-12"> <!-- rodapé -->
             <img src="../img/alcina.jpg" class="logo-alcina">
