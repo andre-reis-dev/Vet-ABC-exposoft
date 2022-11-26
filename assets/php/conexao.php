34f1dea03be10d1b159823eb4911439575ac739b
Barbara-barbato <babi.barbato@gmail.com>
2022-11-25 21:44:53 -0300

    conexao update

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-    $usuario = "root";
-    $senha = ""; 
-    $banco = "vet-abc";
+    $usuario = "u413309708_babi";
+    $senha = "Babi#1234"; 
+    $banco = "u413309708_vetabc";
 
     $conexao=mysqli_connect("localhost",$usuario,$senha,$banco);
 
